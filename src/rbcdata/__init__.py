b205bb51bfc8d3801f05ccd40c5c3df89d85146f
Thorben Markmann <tmarkmann@techfak.uni-bielefeld.de>
2024-02-08 12:41:50 +0100

    Add imports in root module init

---
+++ src/rbcdata/__init__.py
@@ -0,0 +1,17 @@
+from rbcdata.rbc_dataset import RBCDataset
+from rbcdata.utils.rbc_dataset_params import RBCDatasetParams
+from rbcdata.utils.rbc_field import RBCField
+from rbcdata.utils.rbc_simulation_params import RBCSimulationParams
+from rbcdata.utils.rbc_type import RBCType
+from rbcdata.vis.rbc_conv_visualizer import RBCConvectionVisualizer
+from rbcdata.vis.rbc_field_visualizer import RBCFieldVisualizer
+
+__all__ = [
+    "RBCDataset",
+    "RBCType",
+    "RBCField",
+    "RBCDatasetParams",
+    "RBCSimulationParams",
+    "RBCConvectionVisualizer",
+    "RBCFieldVisualizer",
+]
