829900c4638ec2c663d42c035bc756b0554fb562
Thorben Markmann <tmarkmann@techfak.uni-bielefeld.de>
2024-08-09 15:47:35 +0200

    Add checkpoints

--- scripts/cp_baseline_ckps.py
+++ scripts/cp_baseline_ckps.py
@@ -3,20 +3,21 @@ import shutil
 
 import yaml
 
-PATH = "."
-OUTPUT = "./data"
+PATH = "data/checkpoints/08-08-23-04-23"
+OUTPUT = "data"
 dirs = [f.path for f in os.scandir(PATH) if f.is_dir()]
+print(dirs)
 
 for dir in dirs:
     # Load config
-    with open("{dir}/.hydra/config.yml", "r") as file:
+    with open(f"{dir}/.hydra/config.yaml", "r") as file:
         cfg = yaml.safe_load(file)
         ra = cfg["sim"]["ra"]
-        seed = cfg["sim"]["seed"]
+        seed = cfg["seed"]
     # Copy file
     src = f"{dir}/shenfun/checkpoint/RB_2D.chk.h5"
     dest = f"{OUTPUT}/checkpoints/ra{ra}/train/baseline{seed}.chk.h5"
     if os.path.isfile(dest):
         print(f"File '{dest}' already exists! exiting...")
         exit(1)
-    shutil.copyfileobj(src, dest)
+    shutil.copyfile(src, dest)
