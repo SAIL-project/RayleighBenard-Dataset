678c3ec3135caf6c457343e10063b8c49f1478f0
Thorben Markmann <tmarkmann@techfak.uni-bielefeld.de>
2025-01-23 00:34:28 +0100

    Better evaluation script

---
+++ rbcdata/sbsa_eval.py
@@ -0,0 +1,121 @@
+from os.path import join
+
+import hydra
+import matplotlib.animation as animation
+import numpy as np
+import wandb
+import yaml
+from gymnasium.wrappers import FlattenObservation, FrameStackObservation
+from hydra.core.hydra_config import HydraConfig
+from matplotlib import pyplot as plt
+from omegaconf import DictConfig
+from stable_baselines3 import PPO
+from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.logger import configure
+from stable_baselines3.common.vec_env import DummyVecEnv
+from wandb import Video
+
+from rbcdata.env.rbc_env import RayleighBenardEnv
+
+
+@hydra.main(version_base=None, config_path="config", config_name="sbsa_eval")
+def main(cfg: DictConfig) -> None:
+    # Configure logging
+    output_dir = HydraConfig.get().runtime.output_dir
+    # wandb
+    wandb.init(
+        project="sb3-single-agent",
+        config=dict(cfg),
+        sync_tensorboard=True,
+        dir=output_dir,
+        tags=["eval"],
+    )
+    # sb3 logging
+    logger = configure(join(output_dir, "log"), ["stdout", "log", "json", "tensorboard"])
+    logger.info(f"Set log directory to {output_dir}")
+
+    # TODO: new config for evaluation. rn: use train config
+    with open(join(cfg.experiment_dir, ".hydra/config.yaml")) as file:
+        config = DictConfig(yaml.safe_load(file))
+        model_path = join(cfg.experiment_dir, "model/best_model.zip")
+    logger.info(f"Loaded config from {cfg.experiment_dir}/.hydra/config.yaml")
+
+    # Get env, wrappers and policy
+    env = make_vec_env(
+        lambda: FrameStackObservation(
+            FlattenObservation(
+                RayleighBenardEnv(config.test_env, render_mode="rgb_array"),
+            ),
+            stack_size=config.sb3.frame_stack,
+        ),
+        vec_env_cls=DummyVecEnv,
+    )
+    policy = PPO.load(model_path, env=env)
+    policy.set_logger(logger)
+
+    # Enjoy trained agent
+    for idx in range(cfg.nr_episodes):
+        logger.info(f"Evaluating model on episode {idx}")
+        # data holders
+        screens = []
+        actions = []
+        # logging
+        wandb.define_metric(f"ep{idx}_time")
+        wandb.define_metric(f"ep{idx}_nusselt_state", step_metric=f"ep{idx}_time")
+        wandb.define_metric(f"ep{idx}_nusselt_obs", step_metric=f"ep{idx}_time")
+        wandb.define_metric(f"ep{idx}_reward", step_metric=f"ep{idx}_time")
+        # reset env
+        obs = env.reset()
+        dones = np.zeros(1)
+        while not dones.any():
+            action, _ = policy.predict(obs, deterministic=True)
+            obs, rewards, dones, info = env.step(action)
+            # save data
+            screens.append(env.render().transpose(2, 0, 1))
+            actions.append(action.squeeze())
+            # log data
+            wandb.log(
+                {
+                    f"ep{idx}_time": info[0]["t"],
+                    f"ep{idx}_nusselt_state": info[0]["nusselt"],
+                    f"ep{idx}_nusselt_obs": info[0]["nusselt_obs"],
+                    f"ep{idx}_reward": rewards[0],
+                }
+            )
+        # plot data
+        plot_actions(actions, output_dir, idx)
+        wandb.log(
+            {
+                f"ep{idx}_video": Video(np.asarray(screens), fps=1, format="mp4"),
+            }
+        )
+
+
+def plot_actions(actions, out_dir, episode_idx):
+    # Plot nusselt number
+    fig, ax = plt.subplots()
+    # Plot amplitude
+    ax.set_xlabel("segements")
+    ax.set_ylabel("amplitude")
+    ax.set_ylim(-1.1, 1.1)
+    ax.tick_params(axis="y")
+    ax.grid()
+    # plot actions
+    artists = []
+    for action in actions:
+        container = ax.plot(range(len(action)), action, color="blue")
+        artists.append(container)
+
+    ani = animation.ArtistAnimation(fig=fig, artists=artists)
+    writer = animation.FFMpegWriter(fps=2)
+    path = f"{out_dir}/actions_ep{episode_idx}.mp4"
+    ani.save(path, writer=writer)
+    wandb.log(
+        {
+            f"ep{episode_idx}_actions": Video(path, format="mp4"),
+        }
+    )
+
+
+if __name__ == "__main__":
+    main()
