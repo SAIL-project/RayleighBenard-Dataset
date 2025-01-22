0958c60424c2ca305e059fd5282807205cac8e2c
Michiel Straat <michielstraat@gmail.com>
2025-01-22 15:59:00 +0000

    reward shaping

--- rbcdata/env/rbc_env.py
+++ rbcdata/env/rbc_env.py
@@ -47,11 +47,13 @@ class RayleighBenardEnv(gym.Env[RBCAction, RBCObservation]):
         self,
         env_config: Dict,
         render_mode: Optional[str] = None,
+        reward_shaping = False,
     ) -> None:
         """
         Initialize the Rayleigh-Benard environment with the given configuration Dictionary.
         """
         super().__init__()
+        self.reward_shaping = reward_shaping
         # write checkpoint path
         write_checkpoint = env_config.get("write_checkpoint", self.WRITE_CHECKPOINT)
         self.path = "shenfun"
@@ -60,6 +62,7 @@ class RayleighBenardEnv(gym.Env[RBCAction, RBCObservation]):
 
         # initialize from checkpoint path
         self.checkpoint = env_config.get("checkpoint", self.CHECKPOINT)
+        print(self.checkpoint)
         self.load_checkpoint_files = []
         if self.checkpoint is not None:
             self.checkpoint = to_absolute_path(self.checkpoint)
@@ -250,6 +253,17 @@ class RayleighBenardEnv(gym.Env[RBCAction, RBCObservation]):
             neg_nusselt_nr + 3.8
         ) / 3.8  # TODO find out more about what the lowest achievable Nusselt number is
         return reward
+
+    def compute_distance_cells(self) -> float:
+        """
+        Computes the distance between the Bénard cells of the state, given the mid-line temperature"""
+        state = self.get_state()
+        distance = 0
+        T_mid_line = state[RBCField.T][int(self.size_state[0] / 2) - 1]
+        # Find the locations of the cells
+
+
+        return distance
 
     def __get_info(self) -> dict[str, Any]:
         return {
